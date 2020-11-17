5d975617c6cc46291e969057691ab4853c7984ca
michael dilulo <snoleopard01001101@gmail.com>
2020-11-17 12:09:07 -0800

    State for Messages and Input, sendMessage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [input, setInput] = useState('')
+  const [messages, setMessages] = useState([])
+
+  const sendMessage = (event) => {
+
+    setMessages([...messages, input])
+    setInput('')
+
+  }
+
   return (
     <div className="App">
       <h1>Facebook Messenger Clone</h1>
       <h2>React.js and Firebase</h2>
+
+      <input value={input} onChange={event => setInput(event.target.value)} />
+      <button onClick={sendMessage}>Send Message</button>
     </div>
   );
 }
